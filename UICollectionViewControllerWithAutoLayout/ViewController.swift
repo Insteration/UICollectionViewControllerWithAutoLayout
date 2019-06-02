feaec966505c7caffd5fa7d20770b5f8f0709791
Artem <inste@icloud.com>
2019-06-02 13:25:05 +0300

    Initial Commit

---
+++ UICollectionViewControllerWithAutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewControllerWithAutoLayout
+//
+//  Created by Артём Кармазь on 6/2/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
